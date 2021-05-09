cc03882b9a7a908ce81b0cdabe015c56deeb8984
Cristian <c.perezc2018@gmail.com>
2021-05-09 11:27:45 +0200

    Especie y Sustancia

---
+++ src/main/java/com/mi/paquetote/SustanciaActiva.java
@@ -0,0 +1,41 @@
+package com.mi.paquetote;
+
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+public class SustanciaActiva {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String nombre;
+	@ManyToMany
+	private List<Producto> listFitoProds;
+	
+	
+	public SustanciaActiva() {}
+	
+	public SustanciaActiva(String nombre, List<Producto> ls) {
+		this.nombre = nombre;
+		this.listFitoProds = ls;
+	}
+	
+	public void setNombre(String n) {
+		this.nombre = n;
+	}
+	
+	public void setListFitoProds(List<Producto> ls) {
+		this.listFitoProds = ls;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public List<Producto> getListFitoProds(){
+		return this.listFitoProds;
+	}
+}
