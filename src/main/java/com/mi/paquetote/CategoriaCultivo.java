1860a453c3c809a3e2af0c9284faa6c5822a87c8
Heqro <heqromancer@gmail.com>
2021-05-09 11:25:23 +0200

    prueba de push

--- src/main/java/com/mi/paquetote/CategoriaCultivo.java
+++ src/main/java/com/mi/paquetote/CategoriaCultivo.java
@@ -1,9 +1,15 @@
 package com.mi.paquetote;
 
 import java.util.List;
+import javax.persistence.*;
+import org.springframework.data.annotation.Id;
 
+@Entity
 public class CategoriaCultivo {
+	@Id
+	private Long id;
 	private String nombre;
+	@ManyToMany
 	private List<Especie> listaEspecies;
 	
 	public CategoriaCultivo(String n) {
--- src/main/java/com/mi/paquetote/Producto.java
+++ src/main/java/com/mi/paquetote/Producto.java
@@ -1,8 +1,18 @@
 package com.mi.paquetote;
 
+import java.util.List;
+
+import javax.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Producto {
+	@Id
+	private long id;
 	private String nombre;
 	private String URL;
+	@ManyToMany(mappedBy="listFitoProds")
+	private List<Producto> listaProductos;
 	
 	public Producto (String n, String u) {
 		nombre = n;
